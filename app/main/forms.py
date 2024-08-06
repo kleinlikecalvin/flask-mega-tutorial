16b81f2131a437cf4f4f7a1f12bcabb1fb096a48
streetlightkids <ltj_hq@hotmail.com>
2024-08-05 17:30:47 -0700

    Chapter 16 - Complete

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,4 @@
+from flask import request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import ValidationError, DataRequired, Length
@@ -23,6 +24,17 @@ class EditProfileForm(FlaskForm):
                 User.username == self.username.data))
             if user is not None:
                 raise ValidationError(_('Please use a different username.'))
+
+
+class SearchForm(FlaskForm):
+    q = StringField(_l('Search'), validators=[DataRequired()])
+
+    def __init__(self, *args, **kwargs):
+        if 'formdata' not in kwargs:
+            kwargs['formdata'] = request.args
+        if 'meta' not in kwargs:
+            kwargs['meta'] = {'csrf': False}
+        super(SearchForm, self).__init__(*args, **kwargs)
 
 
 class EmptyForm(FlaskForm):
--- app/search.py
+++ app/search.py
@@ -5,25 +5,25 @@ def add_to_index(index, model):
     if not current_app.elasticsearch:
         return
     payload = {}
-    for fields in model.__searchable__:
-        payload[fields] = getattr(model, fields)
+    for field in model.__searchable__:
+        payload[field] = getattr(model, field)
     current_app.elasticsearch.index(index=index, id=model.id, document=payload)
 
 
 def remove_from_index(index, model):
     if not current_app.elasticsearch:
         return
-    current_app.elasticsearch.delete(index=index, model=model.id)
+    current_app.elasticsearch.delete(index=index, id=model.id)
 
 
 def query_index(index, query, page, per_page):
+    print(query)
     if not current_app.elasticsearch:
         return [], 0
     search = current_app.elasticsearch.search(
         index=index,
         query={'multi_match': {'query': query, 'fields': ['*']}},
         from_=(page - 1) * per_page,
-        size=per_page
-    )
+        size=per_page)
     ids = [int(hit['_id']) for hit in search['hits']['hits']]
     return ids, search['hits']['total']['value']
